8bf9c1032f4987cc9743528d606e7a9f99a0821c
Michelle Brien <michelle.brien12@gmail.com>
2017-04-23 19:23:06 +0100

    Twitter functionality - wip

--- Free-code-camp/intermediate-front-end-projects/random-quote-machine/js/main.js
+++ Free-code-camp/intermediate-front-end-projects/random-quote-machine/js/main.js
@@ -1,10 +1,43 @@
+
+
+
+//Generate new quote from API
 $(document).ready(function() {
-    $("button").on("click", function() {
+    $(".new_quote").on("click", function() {
         $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
-            
+        var quote = json.quote;
+        var author = json.author;
+
         $(".quote").html(json.quote);
         $(".author").html(json.author);
+        $('#twitter-share-button').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" ' + author));
+        console.log(quote, author);
         });
     });
 });
+
+
+
+
+
+// Twitter JS function - loads right button https://dev.twitter.com/web/javascript/loading 
+
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
 
