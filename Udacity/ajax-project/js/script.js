e7a75e69764796b9b0e8b88d1d249f2e8b88b14a
Michelle Brien <michelle.brien12@gmail.com>
2017-05-03 19:37:43 +0100

    added nyt api - working in console

--- Udacity/ajax-project/js/script.js
+++ Udacity/ajax-project/js/script.js
@@ -20,7 +20,6 @@ function loadData() {
     var address = streetName + ', ' + cityName;
 
 
-
     // Greeting
     $greeting.text('So you want to live at ' + address + '?');
 
@@ -32,10 +31,28 @@ function loadData() {
     $body.append(backgroundImage);
 
 
+    // NYT AJAX request
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "951b40500d3f459b95d23b5dfdba9a05",
+        'q': cityName
+    });
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function (result) {
+        console.log(result);
+    }).fail(function (err) {
+        throw err;
+    });
 
 
 
-    return false;
+
+
+
+return false;
 };
 
 // When someone hits submit - it will load 'loadData function' above
