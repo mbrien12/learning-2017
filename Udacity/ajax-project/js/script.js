1dcf756cf09989fd5343953a8a2c15cc765daf27
Michelle Brien <michelle.brien12@gmail.com>
2017-05-04 22:11:38 +0100

    added wikipedia api

--- Udacity/ajax-project/js/script.js
+++ Udacity/ajax-project/js/script.js
@@ -31,7 +31,7 @@ function loadData() {
     $body.append(backgroundImage);
 
 
-    // NYT AJAX request
+    // Guardian AJAX request
 
     var guardianUrl = 'https://content.guardianapis.com/search?q=' + cityName + '&order-by=newest&api-key=475649b2-5ea2-4ea3-aa59-2ceeb6bab09e '
 
@@ -45,14 +45,37 @@ function loadData() {
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
 
-            $nytElem.append('<li class="article">' +  '<p>' + article.sectionName + '</p>' +
-             '<a href="' + article.webUrl + '">' + article.webTitle + '</a>' +
+            $nytElem.append('<li class="article">' + '<p>' + article.sectionName + '</p>' +
+                '<a href="' + article.webUrl + '">' + article.webTitle + '</a>' +
                 '</li>');
         };
+
+
+    }).error(function () {
+        $nytHeaderElem.text("Sorry articles can't be loaded right now");
+
+    });
+
+
+    // Wikipedia AJAX request with JSONP
+
+    var wikiRequestTimeout = setTimeout(function () {
+        $wikiElem.text("Failed to get Wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + cityName,
+        dataType: "jsonp",
+        success: function (data) {
+            for (var i = 0; i <= data[1].length - 1; i++) {
+                var pageLink = '<li><a href="' + data[3][i] + '">' + data[1][i] + '</a></li>';
+                $wikiElem.append(pageLink);
+            };
+
+            clearTimeout(wikiRequestTimeout);
+
+        }
     })
-
-
-
 
 
 
