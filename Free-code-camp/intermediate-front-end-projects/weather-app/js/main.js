4eef09ad37f6ea6b7883e81bfbf725bec7c147df
Michelle Brien <michelle.brien12@gmail.com>
2017-05-07 21:08:34 +0100

    working on geolocation

--- Free-code-camp/intermediate-front-end-projects/weather-app/js/main.js
+++ Free-code-camp/intermediate-front-end-projects/weather-app/js/main.js
@@ -2,7 +2,49 @@
 //Get location 
 
 navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position.coords.latitude, position.coords.longitude);
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    var geoCodeAPI = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude + ','+longitude+ '&key=AIzaSyB3mYEF889VBLg_-ATuGqi8Vs9Ri9iSowo';
+
+    $.getJSON(geoCodeAPI, function (data) {
+    console.log(data.results[0].address_components);
+
+var arrAddress = data.results[0].address_components;
+var itemRoute='';
+var itemLocality='';
+var itemCountry='';
+var itemPc='';
+var itemSnumber='';
+
+// iterate through address_component array
+$.each(arrAddress, function (i, address_component) {
+    console.log('address_component:'+i);
+
+    if (address_component.types[0] == "route"){
+        console.log(i+": route:"+address_component.long_name);
+        itemRoute = address_component.long_name;
+    }
+
+    if (address_component.types[0] == "locality"){
+        console.log("town:"+address_component.long_name);
+        var itemLocality = address_component.long_name;
+    }
+
+    //return false; // break the loop  
+    console.log("your city is "+ itemLocality);
+}) 
+ 
+});
+
+
+
+    var city = geoCodeAPI
+
+    
+    console.log(geoCodeAPI);
+    console.log(latitude, longitude);
+    $(".location").text("XX");
 });
 
 
@@ -33,6 +75,7 @@ $.ajax({
 }
 })
 
+// Convert lat and long to city.. in geolocation above
 
 // If temp below X show winter > if temp inbetween X-X show grey and if above X show summer
 
---
+++ Free-code-camp/intermediate-front-end-projects/weather-app/scratchpad.js
@@ -0,0 +1,29 @@
+        $(".metric").on('click',function () {
+            $(".temp").text(data.currently.temperature);
+            $(".metric").text(" F"); // Need to revert back to original state...!!?
+
+            https://api.darksky.net/forecast/ace5824d768c604c262956cc4d59425d/51.520648,-0.066424
+
+            //Get location 
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log(position.coords.latitude, position.coords.longitude);
+
+
+// API variables
+var baseURL = 'https://api.darksky.net/forecast/';
+var key = 'ace5824d768c604c262956cc4d59425d/';
+var lat = position.coords.latitude;
+var long = position.coords.longitude;
+
+var fullURL = baseURL + key + lat + long;
+
+});
+
+
+//basic geolocation
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log(position.coords.latitude, position.coords.longitude);
+});
+
