0f4ae7b6b133ee7da3d78e16712443c12a6efa80
Michelle Brien <michelle.brien12@gmail.com>
2017-05-14 18:59:51 +0100

    trying to sort live status uut

--- Free-code-camp/intermediate-front-end-projects/twitch/js/main.js
+++ Free-code-camp/intermediate-front-end-projects/twitch/js/main.js
@@ -1,18 +1,38 @@
 
-var twitchBase = "https://wind-bow.glitch.me/twitch-api/channels/";
+
+// Get data
+var twitchChannels = "https://wind-bow.glitch.me/twitch-api/channels/";
+var twitchStreams = "https://wind-bow.glitch.me/twitch-api/streams/";
 var channel = ["medrybw", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-for (var i = 0; i < channel.length; i++) 
+for (var i = 0; i < channel.length; i++) {
 
-$.ajax ({
-    url: twitchBase + channel[i],
-    datatype: "jsonp",
-    success: function(data) {
-        console.log (data);
-        var profile = '<img class = "profile" src=" ' + data.logo + '">';
-        var pageLink = '<a href="' +data.url + '"target="_blank">' + data.display_name + '</a>'
-        $("ul").append('<li>' + profile + pageLink + '</li>');
-        
-    }
 
-})
+    // Display each channel in a list
+    $.ajax({
+        url: twitchChannels + channel[i],
+        datatype: "jsonp",
+        success: function (data) {
+            console.log(data);
+            var profile = '<img class = "profile" src=" ' + data.logo + '">';
+            var pageLink = '<a href="' + data.url + '"target="_blank">' + data.display_name + '</a>'
+            $("ul").append('<li>' + profile + pageLink + '</li>');
+
+        }
+    })
+
+
+    // Check if online
+    $.ajax({
+        url: twitchStreams + channel[i],
+        datatype: "jsonp",
+        success: function (data) {
+            if (!data.stream) {
+                console.log("yes");
+                
+                return;
+            }
+
+        }
+    })
+}
